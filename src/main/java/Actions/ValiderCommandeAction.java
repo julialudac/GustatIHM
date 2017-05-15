ac5a340fce1ccfdc732da78b35f65126a313f5e7
wth <tianhao.wu@insa-lyon.fr>
2017-05-15 21:35:40 +0200

    si aucun livreur est disponible, la validation de commande par client va rendre un message lui dit

--- src/main/java/Actions/ValiderCommandeAction.java
+++ src/main/java/Actions/ValiderCommandeAction.java
@@ -5,12 +5,16 @@
  */
 package Actions;
 
+import com.google.gson.JsonObject;
 import com.insa.gustatif.metier.modele.Client;
 import com.insa.gustatif.metier.modele.Commande;
+import com.insa.gustatif.metier.modele.Livreur;
 import com.insa.gustatif.metier.modele.Restaurant;
 import com.insa.gustatif.metier.service.ServiceMetier;
 import java.io.IOException;
+import java.io.PrintWriter;
 import static java.lang.Long.parseLong;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
@@ -24,16 +28,47 @@ import javax.servlet.http.HttpSession;
 public class ValiderCommandeAction extends Action{
 
     @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) {
+    public void execute(HttpServletRequest request, HttpServletResponse reponse) {
         // Recupere les datas de commande
         System.out.println("Je suis dans action ValiderCommandeAction");
         ConnectionSession connectionSession = ConnectionSession.INSTANCE;
-         HttpSession session = request.getSession(true);
+        HttpSession session = request.getSession(true);
         Client client =  connectionSession.getUser(request.getSession().getId());
         //Commande commande = connectionSession.getCommandeByClient(client.getId());
         Commande commande = (Commande) session.getAttribute("commande");
         System.out.println(commande);
-        ServiceMetier.traiterCommande(commande);
+        
+        //pour trouver si il y un livreur disponible
+        List<Livreur> livreurs = ServiceMetier.findAllLivreurs();
+        boolean livreurDisponi = false;
+        for (Livreur livreur : livreurs){
+            if (livreur.getCmdeEnCours()==null){
+                livreurDisponi = true;
+                break;
+            }
+        }
+        System.out.println("livreurDisponi:" + livreurDisponi);
+        
+        reponse.setContentType("text/html;charset=UTF-8");
+        JsonObject jsonMessage = new JsonObject();
+        PrintWriter out = null;    
+        try {
+            out = reponse.getWriter();
+        } catch (IOException ex) {
+            Logger.getLogger(GetRestaurantsAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(livreurDisponi){
+            ServiceMetier.traiterCommande(commande);
+            jsonMessage.addProperty("info", "Votre commande est bien validé");
+            out.println(jsonMessage);
+            out.close();
+        } else {
+            System.out.println("Dans le cas pas de livreur disponible");
+            ServiceMetier.traiterCommande(commande);
+            jsonMessage.addProperty("info", "Aucun livreur n'est actuellement disponible pour traiter votre commande.Veuillez réessayer ultérieurement.");
+            out.println(jsonMessage);
+            out.close();
+        }
         session.setAttribute("commande", commande);
         //connectionSession.enregistrerCommande(request.getSession().getId(),commande.getNumCommande());
     }
